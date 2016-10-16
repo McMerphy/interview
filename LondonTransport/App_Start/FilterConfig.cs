f22ba2310ecb8bef49578c73c244700463841f94
McMerphy <marozaliaksei@gmail.com>
2016-10-16 17:44:18 +0300

    Init

---
+++ LondonTransport/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LondonTransport
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ LondonTransport/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LondonTransport.MvcApplication" Language="C#" %>
